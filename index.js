09c9fd13d3f5f068b0c19e42a0fc18018a0196ee
Gebremeskel <gebre20122@gmail.com>
2021-08-22 06:56:34 -0500

    commit tree d4d0cd563c436ba44ca811243798200f764d52e8

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+function writeCards(array, string) {
+	let new_arr = []
+
+	for (let i = 0; i < array.length; i++) {
+		new_arr.push(`Thank you, ${array[i]}, for the wonderful ${string} gift!`)
+	}
+
+	console.log(new_arr)
+	return new_arr
+}
+
+// writeCards( [ 'Lisa', 'Kaitlin', 'Jan' ], 'surprise' )
+
+function countDown( startingNumber ) {
+    while ( startingNumber > 0 ) {
+      console.log( startingNumber );
+      startingNumber -= 1;
+    }
+    console.log( startingNumber );
+  }
